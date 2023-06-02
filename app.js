7d46aa0e18eca4bc9de4f99e460b0b9ced870ebc
Liberty-slug <gbadamosilateef6@gmail.com>
2023-06-02 21:55:29 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+// Middleware
+app.use(express.static('./public'))
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/v1/tasks', tasks)
+
+const port = process.env.PORT;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
